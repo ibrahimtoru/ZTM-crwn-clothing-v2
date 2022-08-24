433cf41806416b52dcd68ac96e23c64cc8d81e8c
ibrahimtoru <64074146+ibrahimtoru@users.noreply.github.com>
2022-08-24 11:11:42 +0500

    added spinner. migrated all context to redux.

---
+++ src/store/categories/category.selectors.js
@@ -0,0 +1,31 @@
+import { createSelector } from "reselect"; // createSelector memoize selectors
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+// memoized selector
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
+);
+
+// export const selectCategoriesMap = (state) =>
+//   state.categories.categories.reduce((acc, category) => {
+//     const { title, items } = category;
+//     acc[title.toLowerCase()] = items;
+//     return acc;
+//   }, {});
